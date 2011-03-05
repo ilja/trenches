1562d108f574c9e9289ecb94b492437715d9514b
Ilja Krijger <ilja@quittheprogram.org>
2011-03-05 21:38:53 +0100

    add start/end date fields to sprint

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,6 +1,10 @@
 class Sprint
   include Mongoid::Document
   field :name, :type => String
+  field :start_date, :type => Date
+  field :end_date, :type => Date
   embedded_in :project, :inverse_of => :sprints
   references_many :stories
+
+  validates_presence_of :start_date, :end_date, :name
 end
--- spec/models/sprint_spec.rb
+++ spec/models/sprint_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Sprint do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    p = Project.create
+    p.sprints.create
+  end
+
+  it "requires a name" do
+    subject.errors[:name].should include("can't be blank")
+  end
+
+  it "requires a start date" do
+    subject.errors[:start_date].should include("can't be blank")
+  end
+
+  it "requires a end date" do
+    subject.errors[:end_date].should include("can't be blank")
+  end
 end
