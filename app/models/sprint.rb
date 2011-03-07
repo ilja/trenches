d66616a20750c55e6cce79ff2391741a6b936729
Ilja Krijger <ilja@quittheprogram.org>
2011-03-07 15:27:15 +0100

    add needed story points features for burndown to sprint

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -7,4 +7,37 @@ class Sprint
   references_many :stories
 
   validates_presence_of :start_date, :end_date, :name
+  
+  # give the total workdays in this sprint
+  def total_work_days
+    (start_date..end_date).select{|d|(1..5).include?(d.wday)}    
+  end
+  
+  # give the total remaining workdays in this sprint on the given date
+  def remaining_work_days(day)
+    (day+1..end_date).select{|d|(1..5).include?(d.wday)}    
+  end
+  
+  # give the total spent workdays in this sprint on the given date
+  def spent_work_days(day)
+    (start_date..day).select{|d|(1..5).include?(d.wday)}    
+  end
+
+  def total_story_points
+    stories.inject(0) do |sum, story|
+      sum + story.points
+    end
+  end
+
+  def done_story_points(date)
+    stories.done.inject(0) do |sum, story|
+      sum + story.points
+    end
+  end
+
+  def open_story_points(date)
+    stories.any_in(:status => ["open", "active"]).inject(0) do |sum, story|
+      sum + story.points
+    end
+  end
 end
--- spec/models/sprint_spec.rb
+++ spec/models/sprint_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe Sprint do
-  subject do
-    p = Project.create
-    p.sprints.create
+  before(:each) do
+    @p = Project.create
+  end
+
+  subject do    
+    @p.sprints.create
   end
 
   it "requires a name" do
@@ -16,5 +19,49 @@ describe Sprint do
 
   it "requires a end date" do
     subject.errors[:end_date].should include("can't be blank")
+  end
+  
+  it "can give the total work days" do   
+    s = @p.sprints.create(:start_date => Date.new(2011, 03, 07), :end_date => Date.new(2011, 03, 25), :name => "My test sprint")    
+    s.total_work_days.count.should == 15
+  end
+  
+  it "can give the remaining work days" do   
+    s = @p.sprints.create(:start_date => Date.new(2011, 03, 07), :end_date => Date.new(2011, 03, 25), :name => "My test sprint")
+    on_date = Date.new(2011, 03, 17)
+    s.remaining_work_days(on_date).count.should == 6
+  end
+  
+  it "can give the spent work days" do   
+    s = @p.sprints.create(:start_date => Date.new(2011, 03, 07), :end_date => Date.new(2011, 03, 25), :name => "My test sprint")
+    on_date = Date.new(2011, 03, 17)
+    s.spent_work_days(on_date).count.should == 9
+  end
+  
+  describe "with stories" do
+    before(:each) do
+      @sprint = @p.sprints.create(:start_date => Date.new(2011, 03, 07), :end_date => Date.new(2011, 03, 25), :name => "My test sprint")
+      @sprint.stories.create(:name => "Story 1", :points => 2, :status => "open")
+      @sprint.stories.create(:name => "Story 2", :points => 1, :status => "active")
+      @story = @sprint.stories.create(:name => "Story 3", :points => 5)
+    end
+    
+    it "can give the total story points" do
+      @sprint.total_story_points.should == 8
+    end
+  
+    it "can give the total done story points on a given date" do
+      @story.status = "done"
+      @story.save
+      on_date = Date.new(2011, 03, 17) #todo: set done date when marking story as done
+      @sprint.done_story_points(on_date).should == 5
+    end
+    
+    it "can give the total open story points on a given date" do
+      @story.status = "done"
+      @story.save
+      on_date = Date.new(2011, 03, 17) #todo: set done date when marking story as done
+      @sprint.open_story_points(on_date).should == 3
+    end
   end
 end
