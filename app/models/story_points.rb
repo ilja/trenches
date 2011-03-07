19a308bdc8546a5b61b55e8cd497eda8daf33efb
Ilja Krijger <ilja@quittheprogram.org>
2011-03-07 17:40:13 +0100

    change story points to contstants

---
+++ app/models/story_points.rb
@@ -0,0 +1,22 @@
+class StoryPoint
+  ACE = 1
+  TWO = 2
+  THREE = 3
+  FIVE = 5
+  EIGHT = 8
+  TEN = 10
+  KING = 20
+
+	# Give all possible Points.
+	def StoryPoint.all
+		[
+			["Ace", StoryPoint::ACE], 
+			["Two", StoryPoint::TWO], 
+			["Three", StoryPoint::THREE],
+			["Five", StoryPoint::FIVE],
+			["Eight", StoryPoint::EIGHT],
+			["Ten", StoryPoint::TEN],
+			["King", StoryPoint::KING]
+		]
+	end
+end
