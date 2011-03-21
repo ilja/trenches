9c980e4897713170da2c45d5f4d67adf68b0ef1b
Ilja Krijger <ilja@quittheprogram.org>
2011-03-21 22:14:27 +0100

    move status change links to helper

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -4,4 +4,18 @@ module StoriesHelper
 			"(#{story.assigned_to.name})"
 		end
 	end
+
+	def change_status_button(story)
+		html = ""
+		if story.points > StoryPoint::UNKNOWN
+			html << "| " unless story.status == "done"
+			if story.status == "open"				
+	    	html << link_to('Start', start_story_path(story.project, story))
+	    end
+	    if story.status == "active"	    	
+	    	html << link_to('Done', finish_story_path(story.project, story))
+	    end
+	  end
+	  html.html_safe
+	end
 end
