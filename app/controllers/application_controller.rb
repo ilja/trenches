7c2482e12830a133b083a7de4f2478926325f5f1
Ilja Krijger <ilja@quittheprogram.org>
2011-06-08 12:37:58 +0200

    remove not working quickfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   def current_user
     #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-    @current_user ||= Factory.create(:user)
+    @current_user ||= Factory.create(:user) #quick fix for development without inet conn.
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    # auth = request.env["omniauth.auth"]
-    #     user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first || User.create_with_omniauth(auth)
-    user = Factory.create(:user)
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
   end
