3b11328d5f1a392d7b38eff53289bce5954af0e8
Ilja Krijger <ilja@quittheprogram.org>
2011-09-05 09:50:05 +0300

    add assets version

--- config/application.rb
+++ config/application.rb
@@ -49,5 +49,11 @@ module TrenchesRb
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+ 
+    # Change the path that assets are served from
+    # config.assets.prefix     = "/assets"    
   end
 end
