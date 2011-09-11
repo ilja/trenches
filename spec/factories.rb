07cf2f430643ef762d2180b1e4db6ec93aa6dfa8
Ilja Krijger <ilja@quittheprogram.org>
2011-09-11 10:04:51 +0200

    update factories to new syntax

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,19 @@
-Factory.define :project do |f|
-  f.name 'my project'
-end
+FactoryGirl.define do
+  factory :project do
+    name 'my project'
+  end
 
-Factory.define :sprint do |f|
-  f.start_date Time.now - 2.weeks
-  f.end_date Time.now + 2.weeks
-  f.name 'my sprint'
-end
+  factory :sprint do
+    start_date Time.now - 2.weeks
+    end_date Time.now + 2.weeks
+    name 'my sprint'
+  end
 
-Factory.define :story do |f|
-  f.name 'my story'
-end
+  factory :story do
+    name 'my story'
+  end
 
-Factory.define :user do |f|
-  f.name 'user'
+  factory :user do
+    name 'user'
+  end
 end
