2ba8a90ba52b1aa420179f3d3710f1038e7ba043
Ilja Krijger <ilja@quittheprogram.org>
2012-07-25 15:06:10 +0200

    use dalli / memcached as cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Trenches::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Use dalli / memcached as cache store
+  config.cache_store = :dalli_store
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
